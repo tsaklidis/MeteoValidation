7d4b5c35358b13ed0af99204ecb8f589b0d310f0
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2019-06-18 13:00:20 +0300

    Stamp to log

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 from database import TheDB
 from scrape import meteo_forecast, station_conditions
 
@@ -10,4 +13,9 @@ s_data = station_conditions()
 meteo_info = db.insert_meteo(m_data)
 station_info = db.insert_station(s_data)
 
+t = time.time()
+stamp = datetime.datetime.fromtimestamp(t).strftime('%Y/%m/%d %H:%M:%S')
+
+print('####### ', stamp, ' #######')
 print(meteo_info, station_info, sep='\n')
+print('################################\n\n')
