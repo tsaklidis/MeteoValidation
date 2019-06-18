28798cc89d52e81d338a941e9dcb091d2b77008d
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2019-06-18 17:47:06 +0300

    Times in list

--- src/main.py
+++ src/main.py
@@ -6,9 +6,16 @@ from scrape import meteo_forecast, station_conditions
 
 
 db = TheDB()
+times = ['00:00', '03:00', '06:00', '09:00',
+         '12:00', '15:00', '18:00', '21:00', '17:50']
 
 m_data = meteo_forecast()
 s_data = station_conditions()
+
+while (s_data['time'] not in times):
+    print('Not in times, waiting my time')
+    time.sleep(60)
+    s_data = station_conditions()
 
 meteo_info = db.insert_meteo(m_data)
 station_info = db.insert_station(s_data)
